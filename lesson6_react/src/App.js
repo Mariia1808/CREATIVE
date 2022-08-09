8d1269b16cc482f3c5ea3a604912c187ec056293
Maria1808 <mvbannikova1808@gmail.com>
2022-08-09 22:30:27 +0500

    Добавила дз за 09.08

--- lesson6_react/src/App.js
+++ lesson6_react/src/App.js
@@ -7,7 +7,7 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const tovars = [{'id': 1, 'img': 'https://img.mvideo.ru/Pdb/small_pic/200/30059533b.jpg','name': 'Ноутбук HUAWEI MateBook 14 KLVL-W56W 16+512GB Space Grey', 'rate': 4.2, 'price': 69999, 'comment': 17, 'bonus':2100},
 {'id': 2, 'img': 'https://img.mvideo.ru/Pdb/small_pic/200/30060665b.jpg', 'name': 'Монитор Rombica SkyView M23-MF (MUT-002)', 'rate': 4.4, 'price': 7899, 'comment': 19, 'bonus':237},
@@ -18,10 +18,10 @@ const tovars = [{'id': 1, 'img': 'https://img.mvideo.ru/Pdb/small_pic/200/300595
 {'id': 7, 'img': 'https://img.mvideo.ru/Pdb/small_pic/200/20078604b.jpg', 'name': 'Сплит-система Electrolux EACS-09HAR_A/N3_21Y', 'rate': 3.3, 'price': 29999, 'comment': 3, 'bonus': 900},]
 
 
-const Card = ({tovar=[]}) =>{
+const Card = ({tovar={}, bonus}) =>{
     const [like, setLike] = useState(false)
     return(
-        <div className="card">
+        <div key={tovar.name.toString()} className="card">
             <img className='img' src = {tovar.img}/>
             <p>{tovar.name}</p>
             <div className='rate_com'>   
@@ -29,7 +29,7 @@ const Card = ({tovar=[]}) =>{
                 <text>{tovar.comment} отзыва</text>
             </div>
             <p>{tovar.price} &#8381;</p>
-            <p>+{tovar.bonus} бонусных рублей</p>
+            <p>+{bonus} бонусных рублей</p>
             <div className='button'>
             <Button variant="contained" title="Добавить в корзину">
                 <ShoppingCartOutlinedIcon />
@@ -49,21 +49,77 @@ const Card = ({tovar=[]}) =>{
     );
 }
 
+class CardTwo extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {like: false};
+      
+    }
+
+    handleChange(e) {
+        this.setState({like: !this.state.like})
+    }
+    
+    render() {
+        const like = this.state.like;
+        const tovar = {} && this.props.tovar;
+        const bonus = this.props.bonus;
+        return(
+            <div key={tovar.name.toString()} className="card">
+                <img className='img' src = {tovar.img}/>
+                <p>{tovar.name}</p>
+                <div className='rate_com'>   
+                    <text id='star'><StarOutlinedIcon/>{tovar.rate}</text>
+                    <text>{tovar.comment} отзыва</text>
+                </div>
+                <p>{tovar.price} &#8381;</p>
+                <p>+{bonus} бонусных рублей</p>
+                <div className='button'>
+                <Button variant="contained" title="Добавить в корзину">
+                    <ShoppingCartOutlinedIcon />
+                </Button>
+                <IconButton onClick={this.handleChange} title="Добавить в избранное">
+                    {like? 
+                        <FavoriteIcon/>
+                        :
+                        <FavoriteBorderOutlinedIcon />
+                    }
+                </IconButton>
+                <IconButton title="Добавить в сравнение">
+                    <QueryStatsOutlinedIcon />
+                </IconButton>
+                </div>
+            </div>
+        );
+    }
+  }
+
 const CardList = () =>{
     return(
-        <div className="card_list">
-            {tovars.map(i=>
-                <Card tovar={i}/> 
-            )}
-        </div>
+        <>
+            <div className="card_list">
+                {tovars.map(data=>
+                    <Card tovar={data} bonus={data.bonus}/> 
+                )}
+            </div>
+            <div className="card_list">
+                {tovars.map(data=>
+                    <CardTwo tovar={data} bonus={data.bonus}/>
+                )}
+            </div>
+        </>
     );
 }
 
+// в key лучше указывать постоянное значение, которое будет одноначно идентифицировать его среди других, а индексы лучше нге использовать
+// так как порядок элементов может поменяться
+
 function App() {
   return (
-    <div>
+    <>
       <CardList/>
-    </div>
+    </>
   );
 }
 
